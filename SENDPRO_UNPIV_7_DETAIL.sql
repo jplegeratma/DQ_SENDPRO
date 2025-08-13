de69bd119e2892545a92f4ba0fae1cf5b0241011
jplegeratma <jp.leger@mass.gov>
2025-08-13 11:01:50 -0400

    SENDPRO_UNPIV_7_DETAIL_11.sql 8/7/25

--- SENDPRO_UNPIV_7_DETAIL.sql
+++ SENDPRO_UNPIV_7_DETAIL.sql
@@ -1,13 +1,241 @@
+-- 8/7/25
 
-select distinct filename
-from MHTEAM.DWDQ.INF_B_SENDPRO_CLAIMS_DQ_7_837M_UNPIV_DETAIL
-order by filename;
+--select count(1)
+--from (
+SELECT *
+FROM (
+  SELECT *,
+                                            RANK ()
+                                            OVER (PARTITION BY RUN_DATE,
+                                                               FILENAME,
+                                                               CLAIM_TYPE,
+                                                               RECORD_TYPE,
+                                                               MEASURE
+                                                  ORDER BY
+                                                               RUN_DATE,
+                                                               FILENAME,
+                                                               CLAIM_TYPE,
+                                                               RECORD_TYPE,
+                                                               MEASURE,
+                                                               TYPE,
+                                                               PATIENTCONTROLNUM,
+                                                               NUMDTL)    AS rnk
+
+  FROM (
+    SELECT *
+    FROM INF_B_SENDPRO_CLAIMS_DQ_7_837I_UNPIV_DETAIL
+    WHERE NUMDTL = 1
+    )
+)
+WHERE rnk <= 10
+ORDER BY 
+                                                               RUN_DATE,
+                                                               FILENAME,
+                                                               CLAIM_TYPE,
+                                                               RECORD_TYPE,
+                                                               MEASURE,
+                                                               TYPE,
+                                                               PATIENTCONTROLNUM,
+                                                               RNK
+
+--)
+
+limit 1000;
+
+
+-------------
+/*
+SELECT 
+FILENAME,
+                                                               CLAIM_TYPE,
+                                                               RECORD_TYPE,
+                                                               MEASURE,
+                                                               TYPE,
+                                                               COUNT(RUN_DATE)
+FROM (
+*/
+
+--SELECT COUNT(1)
+--FROM (
+
+SELECT *
+FROM (
+    SELECT *,
+                                        RANK ()
+                                            OVER (PARTITION BY RUN_DATE,
+                                                               FILENAME,
+                                                               CLAIM_TYPE,
+                                                               RECORD_TYPE,
+                                                               MEASURE
+                                                  ORDER BY
+                                                               RUN_DATE,
+                                                               FILENAME,
+                                                               CLAIM_TYPE,
+                                                               RECORD_TYPE,
+                                                               MEASURE,
+                                                               TYPE,
+                                                               PATIENTCONTROLNUM,
+                                                               NUMDTL)    AS rnk
+                                   FROM INF_B_SENDPRO_CLAIMS_DQ_7_837I_UNPIV_DETAIL
+    )
+WHERE rnk <= 250
+AND NUMDTL = 1
+
+ORDER BY 
+                                                               RUN_DATE,
+                                                               FILENAME,
+                                                               CLAIM_TYPE,
+                                                               RECORD_TYPE,
+                                                               MEASURE,
+                                                               TYPE,
+                                                               PATIENTCONTROLNUM,
+                                                               RNK
+limit 1000;
+
+--);
+-- 48,076 rnk 25
+-- 279,253 rnk 250
+/*
+)
+GROUP BY
+FILENAME,
+                                                               CLAIM_TYPE,
+                                                               RECORD_TYPE,
+                                                               MEASURE,
+                                                               TYPE
+ORDER BY
+FILENAME,
+                                                               CLAIM_TYPE,
+                                                               RECORD_TYPE,
+                                                               MEASURE,
+                                                               TYPE;
+*/
+
 
 
 select count(1)
-from MHTEAM.DWDQ.INF_B_SENDPRO_CLAIMS_DQ_7_837M_UNPIV_DETAIL
+FROM INF_B_SENDPRO_CLAIMS_DQ_7_837I_UNPIV_DETAIL
+WHERE NUMDTL = 1
+
+--limit 10;
+-- 47,131,476
+-- 8,950,830                                                               
+                                                               
+-- 6/20
+
+select count(1) from (
+select DISTINCT RUN_DATE, FILENAME, CLAIM_TYPE, RECORD_TYPE, patientcontrolnum, numdtl
+from MHTEAM.DWDQ.INF_SENDPRO_CLAIMS_DQ_QA_7_QA
+);
+
+
+DROP VIEW MHTEAM.DWDQ.INF_B_SENDPRO_CLAIMS_DQ_7_837I_DETAIL
+
+/*
+CREATE VIEW MHTEAM.DWDQ.INF_B_SENDPRO_CLAIMS_DQ_7_837I_DETAIL
+AS
+SELECT DISTINCT RUN_DATE, FILENAME, CLAIM_TYPE, RECORD_TYPE, patientcontrolnum, numdtl
+FROM MHTEAM.DWDQ.INF_SENDPRO_CLAIMS_DQ_QA_7_QA
+ORDER BY RUN_DATE, FILENAME, CLAIM_TYPE, RECORD_TYPE;
+*/
+
+CREATE VIEW MHTEAM.DWDQ.INF_B_SENDPRO_CLAIMS_DQ_7_837I_DETAIL
+AS
+select 
+	RUN_DATE,
+	FILENAME,
+	CLAIM_TYPE,
+	RECORD_TYPE,
+	PATIENTCONTROLNUM,
+	NUMDTL,
+	CLAIMFREQUENCYCODE1X,
+	STATEMENTDTP1X,
+	ADMISSIONDTP1X,
+	ADMITTINGDIAGNOSISCODE1X,
+	FACILITYTYPECODE1X,
+	ADMISSIONTYPECODE1X,
+	ADMISSIONSOURCECODE1X,
+	SVCLINECHARGEAMT1X,
+	SVCLINEREVENUECODE1X,
+	OCCURRENCECODE1X,
+	OCCURRENCESPANCODE1X,
+	VALUECODE1X,
+	CONDITIONCODE1X,
+	PATIENTSTATUSCODE1X,
+	PRINCIPALDIAGNOSISCODE1X,
+	ICD10DIAGNOSIS_CODE1X,
+	MULTIPLEPROCEDURECODE1X,
+	CDT_CODE1X,
+	CPT_CODE1X,
+	HIPPS_CODE1X,
+	SVCLINEPROCMOD1X,
+	SVCLINEADJREVENUECODE1X,
+	PROCEDURECODE1X,
+	SVCLINEADJUDICATIONPROCMOD1X,
+	BILLINGPROVNPI1X,
+	BILL_PROVIDERINTERNALID1X,
+	BILL_PROVIDERPIDSL1X,
+	AT_ATTENDINGPROVNPI1X,
+	AT_PROVIDERINTERNALID1X,
+	AT_PROVIDERPIDSL1X,
+	REF_REFERRINGPROVNPI1X,
+	REF_PROVIDERINTERNALID1X,
+	REF_PROVIDERPIDSL1X,
+	REN_RENDERINGPROVNPI1X,
+	REN_PROVIDERINTERNALID1X,
+	REN_PROVIDERPIDSL1X,
+	OOP_OTHEROPERPROVNPI1X,
+	OOP_PROVIDERINTERNALID1X,
+	OOP_PROVIDERPIDSL1X,
+	OP_OPERATINGPROVNPI1X,
+	OP_PROVIDERINTERNALID1X,
+	OP_PROVIDERPIDSL1X
+FROM MHTEAM.DWDQ.INF_SENDPRO_CLAIMS_DQ_QA_7_QA
+ORDER BY RUN_DATE, FILENAME, CLAIM_TYPE, RECORD_TYPE;
+
+select count(1) from MHTEAM.DWDQ.INF_SENDPRO_CLAIMS_DQ_QA_7_QA;
+
+
+--------
+
+truncate table INF_B_SQL_ERROR;
+
+select * from INF_B_SQL_ERROR;
+
+
+select distinct filename
+from MHTEAM.DWDQ.INF_B_SENDPRO_CLAIMS_DQ_7_837I_UNPIV_DETAIL
+order by filename;
+
+select count(1)
+from MHTEAM.DWDQ.INF_B_SENDPRO_CLAIMS_DQ_7_837I_UNPIV_DETAIL
+where type != 'VALID';
+
+select count(1)
+from MHTEAM.DWDQ.INF_B_SENDPRO_CLAIMS_DQ_7_837I_UNPIV_DETAIL
 where filename = 
-'110025617d_pacdrp_11042025204600_test_de_4f8171fd-f9ac-40ce-9f8f-8de07797ef8e.xml'
+--NCPDP'110088791b_pacdrd_12032025161702_sit_pd_30302539-9d7a-4a48-8f09-8ef9eaa7f71c.xml'
+--D'110031447a_pacdrd_03032025155959_test_de_5256a86b-e060-42c7-93eb-85e98a8b3b70.xml'
+--M'110031447b_pacdrp_04082025130110_test_pd_587e39fa-99ef-4bdf-bd24-8fc9ab2573a1.xml'
+'110031447b_pacdri_02272025175541_test_pd_4e8fcb4d-61e4-4a7b-bdfe-77362f81a5e4.xml';
+
+
+select *
+from MHTEAM.DWDQ.INF_B_SENDPRO_CLAIMS_DQ_7_QA_UNPIV
+where filename = 
+'110031447b_pacdri_02272025175541_test_pd_4e8fcb4d-61e4-4a7b-bdfe-77362f81a5e4.xml'
+and measure = 'OTHER_PROVIDER_NPI';
+
+
+MHTEAM.DWDQ.INF_B_SENDPRO_CLAIMS_DQ_7_QA
+
+select count(*) -- DISTINCT *
+from MHTEAM.DWDQ.INF_SENDPRO_CLAIMS_DQ_QA_7M_QA
+where filename = 
+--'110088791b_pacdrd_12032025161702_sit_pd_30302539-9d7a-4a48-8f09-8ef9eaa7f71c.xml'
+--'110031447a_pacdrd_03032025155959_test_de_5256a86b-e060-42c7-93eb-85e98a8b3b70.xml'
+'110031447b_pacdrp_04082025130110_test_pd_587e39fa-99ef-4bdf-bd24-8fc9ab2573a1.xml'
+
 
 GRANT SELECT ON MHTEAM.DWDQ.INF_B_SENDPRO_CLAIMS_DQ_7_837I_UNPIV_DETAIL TO DI_TEAM_ROLE;
 GRANT SELECT ON MHTEAM.DWDQ.INF_B_SENDPRO_CLAIMS_DQ_7_837M_UNPIV_DETAIL TO DI_TEAM_ROLE;
